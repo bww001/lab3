a3f16dd0a5322287638760a958d07940e2f18907
Briana <bww001@ucsd.edu>
2017-01-26 00:14:11 -0800

    Did lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,28 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").click(function(e) {
+		  $('.jumbotron h1').text("Javascript has taken control");
+		  $("#textjs").text("Please wait...");
+		  $(".jumbotron p").toggleClass("active");
+		});
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+  function projectClick(e) {
+    //Cancel the default action, prevents the page from reloading
+    e.preventDefault();
+
+  var containingProject = $(this).closest(".project");
+  var description = $(containingProject).find(".project-description");
+      if (description.length == 0) {
+        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+      } 
+       else {
+         description.fadeOut();
+	 }
+  }
+  $("a.thumbnail").click(projectClick);
+  
 }
