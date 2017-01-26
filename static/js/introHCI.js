a45d929ec3f51cac2102d91ae54f94a2cf0f4917
Briana <bww001@ucsd.edu>
2017-01-26 01:10:01 -0800

    fixed testjs

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,12 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-		$("#testjs").click(function(e) {
-		  $('.jumbotron h1').text("Javascript has taken control");
-		  $("#textjs").text("Please wait...");
-		  $(".jumbotron p").toggleClass("active");
-		});
+	  $('.jumbotron h1').text("Javascript has taken control");
+	  $("#testjs").text("Please wait...");
+	  $(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
